6a385da7b78ce66bec921dfa8cd4d0fa4b166215
Chiran_105626 <Chiran_105626@105626-002L>
2022-12-11 15:15:11 +0530

    init

---
+++ src/main/java/com/chiran/paymentmanagementservice/PaymentManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chiran.paymentmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentManagementServiceApplication.class, args);
+	}
+
+}
