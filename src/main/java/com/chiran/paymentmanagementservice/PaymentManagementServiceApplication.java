276223db8f3ba24028a64271198baf60ebd7e9de
chiranwettewa <chiranwettewa@gmail.com>
2022-12-12 16:43:30 +0530

    init

--- src/main/java/com/chiran/paymentmanagementservice/PaymentManagementServiceApplication.java
+++ src/main/java/com/chiran/paymentmanagementservice/PaymentManagementServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PaymentManagementServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentManagementServiceApplication.class, args);
 	}
